da7581de82d238dfd4d82ef1272e8b75afa0bd58
Jonathon Sun <77643405+Jonathonsun03@users.noreply.github.com>
2022-11-01 14:46:39 -0400

    Fetch is working with no inputs

--- site/js/main.js
+++ site/js/main.js
@@ -6,3 +6,9 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '© OpenStreetMap',
 }).addTo(voterCanvassingMap);
     
+fetch('data/voters_lists/0101.csv')
+.then(resp => resp.text())
+.then(text => {
+    const data = Papa.parse(text, { header: true});
+    console.log(data);
+});
